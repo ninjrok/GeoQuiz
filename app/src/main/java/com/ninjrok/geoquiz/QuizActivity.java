012cbee9bc64b6b7964d0a7b13edba36ed628e6b
Niranjan <niranjanraok.mec@gmail.com>
2016-06-16 11:34:33 +0530

    wait until loading completes

--- app/src/main/java/com/ninjrok/geoquiz/QuizActivity.java
+++ app/src/main/java/com/ninjrok/geoquiz/QuizActivity.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class QuizActivity extends AppCompatActivity {
@@ -117,7 +116,7 @@ public class QuizActivity extends AppCompatActivity {
             progDialog.setMessage("Loading");
             progDialog.setIndeterminate(false);
             progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progDialog.setCancelable(true);
+            progDialog.setCancelable(false);
             progDialog.show();
         }
 
